f2b703ece4f7de4f532c2fb8799679b2ba9f9d6d
p0nyqz <ponyquiz@gmail.com>
2023-01-05 23:56:45 +0100

    class

--- objects.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ objects.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -1,7 +1,26 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+// class syntax
+class ClassName {
+    // class props and methods
+}
 
-var greeting = "Hello, playground"
+class Chessman {
+    let type: String
+    let color: String
+    var coordinates: (String, Int)? = nil
+    let figureSymbol: Character
+    
+    init(type: String, color:String, figure: Character) {
+        self.type = type
+        self.color = color
+        figureSymbol = figure
+    }
+    
+    func describe(){
+        print("The chessman is \(type), \(color) color and symbol \(figureSymbol)")
+    }
+}
 
-//: [Next](@next)
+var kingWhite = Chessman(type:"king", color:"white", figure: "\u{2654}")
+kingWhite.describe()
