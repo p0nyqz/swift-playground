151ecc0ee628be153fdc848319b71ad66d7ae884
p0nyqz <ponyquiz@gmail.com>
2023-01-06 02:48:36 +0100

    class funcs

--- objects.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ objects.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -5,22 +5,51 @@ class ClassName {
     // class props and methods
 }
 
+
 class Chessman {
-    let type: String
-    let color: String
+    // chessman type
+    enum ChessmanType {
+        case king, castle, bishop, pawn, knight, queen
+    }
+
+    // chessman color
+    enum ChessmanColor {
+        case black, white
+    }
+
+    let type: ChessmanType
+    let color: ChessmanColor
     var coordinates: (String, Int)? = nil
     let figureSymbol: Character
     
-    init(type: String, color:String, figure: Character) {
+    init(type: ChessmanType, color:ChessmanColor, figure: Character) {
         self.type = type
         self.color = color
         figureSymbol = figure
     }
     
+    init(type: ChessmanType, color:ChessmanColor, figure: Character, coordinates: (String, Int)) {
+        self.type = type
+        self.color = color
+        figureSymbol = figure
+        setCoordinates(char: coordinates.0, num: coordinates.1)
+    }
+    
+    // coordinates setup
+    func setCoordinates(char c:String, num n:Int) {
+        coordinates = (c, n)
+    }
+    
+    // take a figure from board
+    func kill(){
+        coordinates = nil
+    }
+    
     func describe(){
-        print("The chessman is \(type), \(color) color and symbol \(figureSymbol)")
+        print("The chessman is \(type), \(color) color and symbol \(figureSymbol) and coordinates: \(String(describing: coordinates))")
     }
 }
 
-var kingWhite = Chessman(type:"king", color:"white", figure: "\u{2654}")
+var kingWhite = Chessman(type:.king, color:.white, figure: "\u{2654}", coordinates: ("A", 6))
+//kingWhite.setCoordinates(char: "E", num: 1)
 kingWhite.describe()
