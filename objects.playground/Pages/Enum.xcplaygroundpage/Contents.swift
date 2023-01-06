4b84c5cfec47696576bf7ef1f0e0e8dd90a0442e
p0nyqz <ponyquiz@gmail.com>
2023-01-06 20:40:52 +0100

    enum: currency, dollarCountries, smiles, arithmetic expressions

--- objects.playground/Pages/Enum.xcplaygroundpage/Contents.swift
+++ objects.playground/Pages/Enum.xcplaygroundpage/Contents.swift
@@ -27,7 +27,7 @@ let currency1 = CurrencyUnit.rouble
 // let currency2 = "rouble”
 
 
-
+/// Countries currency
 enum DollarCountries {
     case usa
     case canada
@@ -42,9 +42,80 @@ enum AdvancedCurrencyUnit {
 
 var dollarCurrency: AdvancedCurrencyUnit = .dollar(nation: .usa, shortName: "USD")
 
-
 switch dollarCurrency {
     case .rouble:print("Rouble")
     case let .euro(countries, shortname):print("Euro, Countries: \(countries). Краткое именование: \(shortname)")
     case .dollar(let nation, let shortname):print("Dollar, \(nation). Short description: \(shortname)")
 }
+
+
+/// Smiles enum
+enum Smile: String {
+    case joy = ":)"
+    case laugh = ":D"
+    case sorrow = ":("
+    case surprise = "o_O"
+    // computed property
+    var description: String {return self.rawValue}
+    func about() {
+        print ("Enum contains batch of smiles")
+    }
+    func descriptionValue() -> Smile {
+        return self
+    }
+    func descriptionRawValue() -> String {
+        return rawValue
+    }
+}
+
+let iAmHappy = Smile.joy
+print(iAmHappy.rawValue)
+
+let mySmile:Smile = .sorrow
+print(mySmile.description)
+mySmile.about()
+
+var otherSmile = Smile.joy
+print(otherSmile.descriptionValue())
+print(otherSmile.descriptionRawValue())
+
+
+/// Simple Arithmetic Expression Recursion
+enum ArithmeticExpression1 {
+    case addition(Int, Int)
+    case substraction(Int, Int)
+    func evaluate() -> Int {
+        switch self {
+        case .addition(let left, let right):
+            return left + right
+        case .substraction(let left, let right):
+            return left - right
+        }
+    }
+}
+
+let expr = ArithmeticExpression1.addition(10, 14).evaluate()
+print(expr)
+
+/// Complex Arithmetic Expression Recursion
+enum ArithmeticExpression {
+    case number( Int )
+    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+    indirect case subtraction(ArithmeticExpression, ArithmeticExpression)
+    func evaluate( _ expression: ArithmeticExpression? = nil) -> Int {
+        switch expression ?? self {
+        case let .number( value ):
+            return value
+        case let .addition(valueLeft, valueRight):
+            return evaluate( valueLeft ) + evaluate( valueRight )
+        case .subtraction( let valueLeft, let valueRight ):
+            return evaluate( valueLeft ) - evaluate( valueRight )
+        }
+    }
+}
+
+let hardExpr = ArithmeticExpression
+    .addition( .number(20),
+    .subtraction( .number(10), .number(34)))
+    .evaluate()
+print(hardExpr)
