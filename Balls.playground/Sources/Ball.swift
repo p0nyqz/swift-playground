cd1086bd645b53b0012f07e0fc6a8b6f23f3a519
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-08-07 03:54:52 +0200

    feat: [Balls] - BallProtocol, Ball class implemented

---
+++ Balls.playground/Sources/Ball.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+protocol BallProtocol {
+    init(color: UIColor, radius: Int, coordinates: (x: Int, y: Int))
+}
+
+public class Ball: UIView, BallProtocol {
+    required public init(color: UIColor, radius: Int, coordinates: (x: Int, y: Int)) {
+    // create graphical rectangle
+    super.init(frame:
+                CGRect(x: coordinates.x,
+                       y: coordinates.y,
+                       width: radius * 2,
+                       height: radius * 2))
+    // rounded corners
+    self.layer.cornerRadius = self.bounds.width / 2.0
+    // change background color
+    self.backgroundColor = color
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
