4aff2e1a0af2da6f6306429306c8c9942f5b0ccf
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-08-11 16:05:36 +0200

    feat: [SquareArea] init and added balls on area

---
+++ Balls.playground/Sources/SquareArea.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+protocol SquareAreaProtocol {
+    init(size: CGSize, color: UIColor)
+    // init balls in area
+    func setBalls(withColors: [UIColor], andRadius: Int)
+}
+
+public class SquareArea: UIView, SquareAreaProtocol {
+    required public init(size: CGSize, color: UIColor) {
+    // collision detector handler
+    collisionBehavior = UICollisionBehavior(items: [])
+    // build rectangular graphical area
+    super.init(frame:
+        CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        // change bg color
+        self.backgroundColor = color
+    collisionBehavior.setTranslatesReferenceBoundsIntoBoundary(with: UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1))
+    animator = UIDynamicAnimator(referenceView: self)
+    animator?.addBehavior(collisionBehavior)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setBalls(withColors ballsColor: [UIColor], andRadius radius: Int) {
+        for (index, oneBallColor) in ballsColor.enumerated() {
+            let coordinateX = 10 + (2 * radius) * index
+            let coordinateY = 10 + (2 * radius) * index
+            let ball = Ball(color: oneBallColor,
+                            radius: radius,
+                            coordinates: (x: coordinateX, y: coordinateY))
+            self.addSubview(ball)
+            self.balls.append(ball)
+            collisionBehavior.addItem(ball)
+        }
+    }
+    
+    // balls collection
+    private var balls: [Ball] = []
+    // graphical objects animator
+    private var animator: UIDynamicAnimator?
+    // balls movement handler
+    private var snapBehavior: UISnapBehavior?
+    // collision counter
+    private var collisionBehavior: UICollisionBehavior
+}
+
+
+
