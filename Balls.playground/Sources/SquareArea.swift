23898639e555f8c86875d2341640e2d5b8ea5b4b
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-08-12 03:05:14 +0200

    feat: [SquareArea] touches and collisions added to balls

--- Balls.playground/Sources/SquareArea.swift
+++ Balls.playground/Sources/SquareArea.swift
@@ -47,15 +47,53 @@ public class SquareArea: UIView, SquareAreaProtocol {
         }
     }
     
-    // balls collection
-    private var balls: [Ball] = []
-    // graphical objects animator
-    private var animator: UIDynamicAnimator?
-    // balls movement handler
-    private var snapBehavior: UISnapBehavior?
-    // collision counter
-    private var collisionBehavior: UICollisionBehavior
+    //    public func setBalls(withColors ballsColor: [UIColor], andRadius radius: Int) {
+    //        // перебираем переданные цвета
+    //        // один цвет — один шарик
+    //        for (index, oneBallColor) in ballsColor.enumerated() {
+    //            // рассчитываем координаты левого верхнего угла шарика
+    //            let coordinateX = 10 + (2 * radius) * index
+    //            let coordinateY = 10 + (2 * radius) * index
+    //            // создаем экземпляр сущности "Шарик"
+    //            let ball = Ball(color: oneBallColor,
+    //                            radius: radius,
+    //                            coordinates: (x: coordinateX, y: coordinateY))
+    //            // добавляем шарик в текущее отображение (в состав площадки)
+    //            self.addSubview(ball)
+    //            // добавляем шарик в коллекцию шариков
+    //            self.balls.append(ball)
+    //            // добавляем шарик в обработчик столкновений
+    //            collisionBehavior.addItem(ball)
+    //        }
+    //    }
+    
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location ( in: self)
+            for ball in balls {
+                if (ball.frame.contains( touchLocation)) {
+                    snapBehavior = UISnapBehavior(item: ball, snapTo: touchLocation)
+                    snapBehavior?.damping = 0.5
+                    animator?.addBehavior(snapBehavior!)
+                }
+            }
+        }
+    }
+    
+    override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location( in: self)
+            if let snapBehavior = snapBehavior {
+                snapBehavior.snapPoint = touchLocation
+            }
+        }
+    }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let snapBehavior = snapBehavior {
+            animator?.removeBehavior(snapBehavior)
+        }
+        snapBehavior = nil
+    }
+    
 }
-
-
-
