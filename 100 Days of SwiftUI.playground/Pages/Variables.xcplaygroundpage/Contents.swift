49789575ee4d9f9182a9eac101745cfa18b2b813
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-11-14 23:34:57 +0100

    100 days: variables: var, let

---
+++ 100 Days of SwiftUI.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Rob"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
