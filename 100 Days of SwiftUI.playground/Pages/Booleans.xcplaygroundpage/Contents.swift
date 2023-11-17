18827a3cd9e09a67114208633ebaca34a72412e9
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-11-17 23:46:28 +0100

    100 days: boolean,exclamation point,toggle

---
+++ 100 Days of SwiftUI.playground/Pages/Booleans.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let goodDog = true
+var gameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+
+// !
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+// .toggle()
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
