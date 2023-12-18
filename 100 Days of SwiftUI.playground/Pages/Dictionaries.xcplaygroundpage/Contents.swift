a839db3a7483c62b4a0d854e1b04ee699825c52d
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2023-12-18 06:46:53 +0100

    100 days: optionals

--- 100 Days of SwiftUI.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
+++ 100 Days of SwiftUI.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
@@ -2,6 +2,53 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// optionals
+let employee = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
 
-//: [Next](@next)
+print(employee["name", default: "Unknown"])
+print(employee["job", default: "Unknown" ])
+print(employee["location", default: "Unknown"])
+
+print(employee["password"])
+print(employee["status"])
+print(employee["manager"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false
+]
+print(hasGraduated["Eric"])
+
+let olimpics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2020: "Tokyo"
+]
+
+print(olimpics[2012, default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+// rewring keys with duplication case
+archEnemies["Batman"] = "Penguin"
+print(archEnemies["Batman", default: "Unknown"])
+
+// count, removeAll()
+print(heights.count)
+heights.removeAll()
+print(heights.count)
+
+// error
+// let abbreviations = ["m": "meters", "km": "kilometers"]
+// let meters = abbreviations["m", default "m"]
