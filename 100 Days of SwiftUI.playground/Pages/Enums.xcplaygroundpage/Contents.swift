1cde70374c40b05b052c970036b19f3ead1acf7d
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-01-11 02:51:27 +0100

    100 days: enums

---
+++ 100 Days of SwiftUI.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+var day = Weekday.monday
+day = Weekday.tuesday
+day = Weekday.friday
+
+enum Month {
+    case jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+}
+
+var months = Month.jan
+months = .feb
+months = .mar
