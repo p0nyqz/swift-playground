58243cc0cdea1eae24a1e2ca4678abb4daf3025c
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-01-11 02:52:25 +0100

    100 days: annotations

--- 100 Days of SwiftUI.playground/Pages/Annotations.xcplaygroundpage/Contents.swift
+++ 100 Days of SwiftUI.playground/Pages/Annotations.xcplaygroundpage/Contents.swift
@@ -2,6 +2,36 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+let surname:String = "Lasso"
+var score:Int = 0
+var otherScore: Double = 0
 
-//: [Next](@next)
+var playerName:String = "Roy"
+var luckyNumber:Int = 13
+let pi: Double = 3.141
+var isAuthenticated = true
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Blues Eye", "Foundation", "Girl, Woman, Other"])
+
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+// Emppty array
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+style = .dark
+
+let username:String
+username = "@twostraws"
+print(username)
+
+//Error
+//let score: Int = "Zero"
+
+
