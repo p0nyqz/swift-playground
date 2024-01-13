82d548ed1d66033ae46a95b811087f2263778049
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-01-13 20:54:14 +0100

    100 days: 2nd checkpoint:unique name numbers in array

--- 100 Days of SwiftUI.playground/Pages/2Checkpoint.xcplaygroundpage/Contents.swift
+++ 100 Days of SwiftUI.playground/Pages/2Checkpoint.xcplaygroundpage/Contents.swift
@@ -2,6 +2,10 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+let albums = ["Red", "Fearless", "Let if Be", "Please Please me", "Help!", "Revolver", "Red"]
+let uniqueAlbums = Set(albums)
 
-//: [Next](@next)
+print("The Beatles has \(albums.count) albums & \(uniqueAlbums.count) with unique names")
+
+
+// This time the challenge is to create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.
